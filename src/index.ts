cc8c6fa30b8a7f8a7434db4a685a13988a74b781
Trần Nguyên <nguyentran0113@gmail.com>
2023-01-21 20:06:55 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const favoriteFruits: string[] = ["apple", "strawberry", "orange"];
+
+function addFruit(fruit: string) {
+    favoriteFruits.push(fruit);
+}
+
+addFruit('sx')
+console.log(favoriteFruits)
